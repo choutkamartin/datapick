4dcb0c8d9d867a9899ef748014a06d2439745e66
Martin Choutka <martafiixek@gmail.com>
2022-01-09 13:41:48 +0100

    Add layouting support to _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-import { Layout } from "components/layout/Layout";
+import PrivateLayout from "components/layout/PrivateLayout";
+import PublicLayout from "components/layout/PublicLayout";
+
+const layouts = {
+  Public: PublicLayout,
+  Private: PrivateLayout,
+};
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  const Layout = layouts[Component.layout] || ((children) => <>{children}</>);
   return (
     <SessionProvider session={session}>
       <Layout>
