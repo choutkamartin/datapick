f3aed7d3a40b3c8b0bffb78af8cd5fc0a85df6ab
Martin Choutka <martafiixek@gmail.com>
2022-01-09 13:54:48 +0100

    Add readOnly styling to input and add Label component

--- components/inputs/Input.jsx
+++ components/inputs/Input.jsx
@@ -1,7 +1,11 @@
-import { ExclamationCircleIcon } from "@heroicons/react/outline";
-import { joinClassNames } from "utils/helpers";
-import { EyeIcon, EyeOffIcon } from "@heroicons/react/outline";
 import { useState } from "react";
+import {
+  EyeIcon,
+  EyeOffIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/outline";
+import Label from "components/inputs/Label";
+import { joinClassNames } from "utils/helpers";
 
 export default function Input({
   label,
@@ -10,6 +14,7 @@ export default function Input({
   id,
   errors,
   type,
+  readOnly,
   ...props
 }) {
   const [mutableType, setMutableType] = useState(type);
@@ -44,26 +49,27 @@ export default function Input({
   };
 
   return (
-    <div className="flex flex-col gap-y-1">
-      <label className="font-medium" htmlFor={id}>
+    <div className="flex flex-col gap-y-1 w-full">
+      <Label htmlFor={id} required={required}>
         {label}
-        {required && <span className="ml-1">*</span>}
-      </label>
+      </Label>
       <div className="relative">
         <input
           className={joinClassNames(
             errors
               ? "ring-1 ring-red-700 focus:ring-red-700"
               : "focus:ring-indigo-600",
-            "min-w-full border-gray-300 focus:border-gray-300 focus:ring-2 rounded bg-slate-50 read-only:cursor-not-allowed"
+            readOnly && "bg-slate-50",
+            "min-w-full border-gray-300 focus:border-gray-300 focus:ring-2 rounded shadow-sm read-only:cursor-not-allowed read-only:text-gray-500"
           )}
           id={id}
           type={mutableType}
+          readOnly={readOnly}
           {...register(id, { required })}
           {...props}
         />
         <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 gap-x-2">
-        <ShowPasswordIcon onClick={() => setPasswordVisibility()} />
+          <ShowPasswordIcon onClick={() => setPasswordVisibility()} />
           {errors && <ExclamationCircleIcon className="w-4 h-4 text-red-700" />}
         </div>
       </div>
