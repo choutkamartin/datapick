a9faf92096fe2dd55891da05fd2c5e36c7f9e998
Martin Choutka <martafiixek@gmail.com>
2022-01-09 23:40:08 +0100

    Add cursor not allowed when button is disabled

--- components/buttons/Button.jsx
+++ components/buttons/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ children, className, ...props }) {
   return (
     <button
       className={joinClassNames(
-        "uppercase whitespace-nowrap flex items-center justify-center px-4 py-2 border border-transparent rounded shadow-sm text-base font-semibold text-white bg-indigo-600 hover:bg-indigo-700",
+        "uppercase whitespace-nowrap flex items-center justify-center px-4 py-2 border border-transparent rounded shadow-sm text-base font-semibold text-white bg-indigo-600 hover:bg-indigo-700 disabled:cursor-not-allowed",
         className
       )}
       {...props}
