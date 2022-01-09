0383bf2e673ee573a2daef169786f7785d8134dd
Martin Choutka <martafiixek@gmail.com>
2022-01-09 13:46:56 +0100

    Add FileInput component

---
+++ components/inputs/FileInput.jsx
@@ -0,0 +1,9 @@
+export default function FileInput({ ...props }) {
+  return (
+    <input
+      type="file"
+      className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-violet-100 file:cursor-pointer focus-visible:outline-0 focus:outline-0"
+      {...props}
+    />
+  );
+}
