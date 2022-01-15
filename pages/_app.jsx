1e074149fd9015cafcc193f01de744cb20515620
Martin Choutka <martafiixek@gmail.com>
2022-01-16 00:44:56 +0100

    Add LabelLayout and refactor

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-import PrivateLayout from "components/layout/PrivateLayout";
-import PublicLayout from "components/layout/PublicLayout";
+import PrivateLayout from "components/layout/private/PrivateLayout";
+import PublicLayout from "components/layout/public/PublicLayout";
+import LabelLayout from "components/layout/label/LabelLayout";
 
 const layouts = {
   Public: PublicLayout,
   Private: PrivateLayout,
+  Label: LabelLayout,
 };
 
 export default function App({
