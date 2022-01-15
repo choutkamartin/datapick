16dbbc5271eab51674d17816572c457bb6050325
Martin Choutka <martafiixek@gmail.com>
2022-01-15 23:56:36 +0100

    Update the sendEmail functions for a better error handling
    Add sendRecoveryEmail function and template

--- utils/email.js
+++ utils/email.js
@@ -1,22 +1,59 @@
 import { SendTemplatedEmailCommand } from "@aws-sdk/client-ses";
 import { sesClient } from "/lib/sesClient.js";
 
-const sendEmail = async (email, verificationLink) => {
+/**
+ * Sends an email with a verification link to the recipient
+ * @param {*} email An email address of the user/recipient of the email
+ * @param {*} verificationLink A verification link of the user
+ * @returns Response from the SES client
+ */
+const sendVerificationEmail = async (email, verificationLink) => {
   const params = {
     Source: process.env.EMAIL_SENDER,
-    Template: "SendVerificationLink",
+    Template: "SendVerificationEmail",
     Destination: {
       ToAddresses: [email],
     },
     TemplateData: `{ "email": "${email}", "verificationLink": "${verificationLink}" }`,
   };
 
+  const command = new SendTemplatedEmailCommand(params);
+
   try {
-    const data = await sesClient.send(new SendTemplatedEmailCommand(params));
-    return data;
+    await sesClient.send(command);
+    return { statusCode: 200 };
   } catch (err) {
-    throw err;
+    return {
+      message: "An error occured when sending e-mail.",
+      statusCode: 500,
+    };
   }
 };
 
-export { sendEmail };
+/**
+ * Sends an email with a recovery link to the recipient
+ * @param {string} email An email address of the user/recipient of the email
+ * @param {string} recoveryLink A recovery link of the user
+ * @returns Response from the SES client
+ */
+const sendRecoveryEmail = async (email, recoveryLink) => {
+  const params = {
+    Source: process.env.EMAIL_SENDER,
+    Template: "SendRecoveryEmail",
+    Destination: {
+      ToAddresses: [email],
+    },
+    TemplateData: `{ "email": "${email}", "recoveryLink": "${recoveryLink}" }`,
+  };
+
+  const command = new SendTemplatedEmailCommand(params);
+
+  try {
+    await sesClient.send(command);
+    return { statusCode: 200 };
+  } catch (err) {
+    return { error: "An error occured when sending e-mail.", statusCode: 500 };
+  }
+};
+
+export { sendVerificationEmail, sendRecoveryEmail };
