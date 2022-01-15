9c0e9eab33d4b1c6b0437dd58e8185faa65fce48
Martin Choutka <martafiixek@gmail.com>
2022-01-16 00:44:38 +0100

    Refactor 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,12 +1,12 @@
-import Container from "components/layout/Container";
-import Title from "components/Title";
+import Container from "components/Container";
+import Heading from "components/Heading";
 import Paragraph from "components/Paragraph";
-import Anchor from "components/links/Anchor";
+import Anchor from "components/Anchor";
 
 function NotFound() {
   return (
     <Container variant="box" className="py-24 flex-col items-start gap-y-4">
-      <Title headingLevel="h1">404</Title>
+      <Heading headingLevel="h1">404</Heading>
       <Paragraph>
         The page you were looking for does not exist. Probably it has been
         deleted?
