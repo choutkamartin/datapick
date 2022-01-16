e402ce1f48509c2fb3bbef42094befb5952c18c5
Martin Choutka <martafiixek@gmail.com>
2022-01-16 01:41:06 +0100

    Refactor/rename ButtonOAuth

--- components/buttons/ButtonOAuth.jsx
+++ components/buttons/ButtonOAuth.jsx
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { joinClassNames } from "utils/helpers";
 
-export default function OAuthButton({ title, icon, className, ...props }) {
+export default function ButtonOAuth({ title, icon, className, ...props }) {
   return (
     <button
       type="button"
       className={joinClassNames(
-        "whitespace-nowrap w-full inline-flex items-center justify-center gap-x-4 px-4 py-2 border border-transparent rounded-sm shadow-sm text-base font-medium",
+        "whitespace-nowrap w-full inline-flex items-center justify-center gap-x-4 px-4 py-2 border border-transparent rounded shadow-sm font-medium",
         className
       )}
       {...props}
