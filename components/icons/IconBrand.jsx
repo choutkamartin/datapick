9190ae5637efb54b523354bc6b983adda4922e5c
Martin Choutka <martafiixek@gmail.com>
2022-01-16 01:49:15 +0100

    Refactor/rename IconBrand

--- components/icons/IconBrand.jsx
+++ components/icons/IconBrand.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Brand({ icon }) {
+export default function IconBrand({ icon }) {
   return (
     <div className="w-12 h-12 flex items-center justify-center bg-indigo-600 hover:bg-indigo-700 rounded-full text-white cursor-pointer">
       <FontAwesomeIcon icon={icon} size="lg" />
