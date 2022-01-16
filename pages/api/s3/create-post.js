9cd95f9be3954615e444bbc82d89d417e65293e9
Martin Choutka <martafiixek@gmail.com>
2022-01-16 01:51:46 +0100

    Change name to key in s3/create-post

--- pages/api/s3/create-post.js
+++ pages/api/s3/create-post.js
@@ -4,10 +4,10 @@ import { s3Client } from "/lib/s3Client.js";
 export default async function handler(req, res) {
   const data = req.body;
   const arr = await Promise.all(
-    data.map(({ name }) =>
+    data.map(({ key }) =>
       createPresignedPost(s3Client, {
         Bucket: process.env.AWS_S3_BUCKET_NAME,
-        Key: name,
+        Key: key,
       })
     )
   );
