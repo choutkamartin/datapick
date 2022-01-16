05ae0d716740a1120e65325b7c315e59f62b4dac
Martin Choutka <martafiixek@gmail.com>
2022-01-16 01:48:07 +0100

    Refactor/rename PrivateSidebar

--- components/layout/private/PrivateSidebar.jsx
+++ components/layout/private/PrivateSidebar.jsx
@@ -1,7 +1,7 @@
-import Title from "components/Title";
-import Anchor from "components/links/Anchor";
+import Title from "components/Heading";
+import Anchor from "components/Anchor";
 
-export default function UserSidebar({ title, data }) {
+export default function PrivateSidebar({ title, data }) {
   return (
     <div className="bg-white px-4 py-8 lg:p-12 w-full lg:w-2/12 border-r border-b-2 lg:border-b-0">
       <Title headingLevel="h3" className="mb-8">
