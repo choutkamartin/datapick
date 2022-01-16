b5154f0ef4e0e5777de24948d6902b258ab5636f
Martin Choutka <martafiixek@gmail.com>
2022-01-16 01:38:54 +0100

    Refactor AlertSuccess component

--- components/alerts/AlertSuccess.jsx
+++ components/alerts/AlertSuccess.jsx
@@ -1,7 +1,8 @@
 import { CheckCircleIcon } from "@heroicons/react/outline";
+import Paragraph from "components/Paragraph";
 import { joinClassNames } from "utils/helpers";
 
-export default function Success({ title, className }) {
+export default function AlertSuccess({ title, className }) {
   return (
     <div
       className={joinClassNames(
@@ -12,7 +13,7 @@ export default function Success({ title, className }) {
       <span className="absolute inset-y-0 left-0 flex items-center ml-4">
         <CheckCircleIcon className="h-4 w-4" />
       </span>
-      <p className="ml-6">{title}</p>
+      <Paragraph className="ml-6">{title}</Paragraph>
     </div>
   );
 }
